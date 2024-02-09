87d18126d4a1688e57d67c25c9f1628d32caa25e
Anees-Alam <anees.rahman@ontariotechu.net>
2024-02-09 17:10:42 -0500

    most changes for 14th

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+const csrf = require('csurf');
 var router = express.Router();
+const app = express();
+
+// Set up CSRF protection middleware
+const csrfProtection = csrf({ cookie: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,5 +36,16 @@ router.get('/about', function(req, res, next) {
     title: 'about me'});
 });
 
+// Define a route handler for the form submission
+router.post('/submit-answer', function(req, res) {
+  // Handle the form submission here
+  // For demonstration purposes, let's just log the submitted data
+  //console.log('Form submitted with data:', req.body);
+  // Send a response
+  //res.send('Form submitted successfully!');
+});
 
 module.exports = router;
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,5 @@ router.get('/', function(req, res, next) {
   res.send('This is for users');
 });
 
+
 module.exports = router;
